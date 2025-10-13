ffd0a399a58391b8254e1efc45dd0cdb0198f143
code3743 <jota.lopez@correounivalle.edu.co>
2025-10-13 10:06:17 -0500

    feat: update user info exports to include loans and history retrieval

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,3 @@
 export { default as getSessionId } from "./opac/get_session_id";
 export { default as authUser } from "./opac/auth_user";
-export { default as getUserInfo } from "./opac/get_user_info";
+export { getUserInfo, getUserLoans, getUserHistory } from "./opac/get_user_info";
